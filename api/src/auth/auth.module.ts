3828f15ec4063f82b25cd9d98127a3e166106a77
Novanni Indi Pradana <novanindipradana@gmail.com>
2025-07-12 00:23:58 +0800

    Use env vars for JWT config

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -4,11 +4,17 @@ import { JwtModule } from "@nestjs/jwt";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { PrismaService } from "../prisma.service"; // ⬅ Import manual
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
-  imports: [JwtModule.register({ secret: "your_secret_key" })],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || "supersecretjwtkey",
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || "1d" },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService], // ⬅ Tambahkan ini
+  providers: [AuthService, PrismaService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
