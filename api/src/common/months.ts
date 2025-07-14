02f01f20e04f590575aad1db8eec49a06bdcc6e3
Novanni Indi Pradana <novanindipradana@gmail.com>
2025-07-14 14:50:53 +0800

    refactor: centralize month names constant

---
+++ api/src/common/months.ts
@@ -0,0 +1,18 @@
+export const MONTHS = [
+  "Januari",
+  "Februari",
+  "Maret",
+  "April",
+  "Mei",
+  "Juni",
+  "Juli",
+  "Agustus",
+  "September",
+  "Oktober",
+  "November",
+  "Desember",
+] as const;
+
+export type MonthName = (typeof MONTHS)[number];
+
+export default MONTHS;
