ff85d2848695943d41b32a7cd38ca2c85f3443ef
Novanni Indi Pradana <novanindipradana@gmail.com>
2025-07-15 13:58:16 +0800

    feat: add full-screen overlay option to Loading

--- web/src/components/Loading.jsx
+++ web/src/components/Loading.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import Spinner from "./Spinner";
 
-export default function Loading() {
+export default function Loading({ fullScreen = false }) {
+  if (fullScreen) {
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+        <div className="text-center space-y-2">
+          <Spinner className="h-6 w-6 mx-auto" />
+          <div>Loading...</div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="py-10 text-center space-y-2">
       <Spinner className="h-6 w-6 mx-auto" />
