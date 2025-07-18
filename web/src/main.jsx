d300d222090ef33d28326e44a3d4da95aa57c132
Novanni Indi Pradana <novanindipradana@gmail.com>
2025-07-18 15:39:38 +0800

    feat: add skeleton component and toast

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/styles/index.css";
 import "sweetalert2/dist/sweetalert2.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./pages/auth/useAuth.jsx";
 import { ThemeProvider } from "./theme/useTheme.jsx";
 import { BrowserRouter } from "react-router-dom";
