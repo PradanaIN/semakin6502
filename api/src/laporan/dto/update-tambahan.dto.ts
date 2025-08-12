b666b0767d0ac64d6c5871849482c7ec6e32bbfe
Novanni Indi Pradana <novanindipradana@gmail.com>
2025-08-12 11:04:31 +0800

    Allow partial updates for tugas tambahan

--- api/src/laporan/dto/update-tambahan.dto.ts
+++ api/src/laporan/dto/update-tambahan.dto.ts
@@ -2,29 +2,34 @@ import { IsDateString, IsOptional, IsString } from "class-validator";
 import { Transform } from "class-transformer";
 
 export class UpdateTambahanDto {
+  @IsOptional()
   @IsString()
-  kegiatanId: string;
+  kegiatanId?: string;
 
   @Transform(({ value }) => (value === "" ? undefined : value))
+  @IsOptional()
   @IsDateString()
-  tanggal: string;
+  tanggal?: string;
 
   @Transform(({ value }) => (value === "" ? undefined : value))
+  @IsOptional()
   @IsString()
-  status: string;
+  status?: string;
 
   @IsOptional()
   @Transform(({ value }) => (value === "" ? undefined : value))
   @IsString()
   buktiLink?: string;
 
+  @IsOptional()
   @Transform(({ value }) => (value === "" ? undefined : value))
   @IsString()
-  deskripsi: string;
+  deskripsi?: string;
 
+  @IsOptional()
   @Transform(({ value }) => (value === "" ? undefined : value))
   @IsString()
-  capaianKegiatan: string;
+  capaianKegiatan?: string;
 
   @IsOptional()
   @Transform(({ value }) => (value === "" ? undefined : value))
