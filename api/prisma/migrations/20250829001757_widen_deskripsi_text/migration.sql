34d6a16700a62c1138c7fb72c300cdc0c647fe67
Novanni Indi Pradana <novanindipradana@gmail.com>
2025-09-01 01:05:07 +0800

    fix migration table name

--- api/prisma/migrations/20250829001757_widen_deskripsi_text/migration.sql
+++ api/prisma/migrations/20250829001757_widen_deskripsi_text/migration.sql
@@ -1,5 +1,5 @@
 -- DropForeignKey
-ALTER TABLE `laporanharian` DROP FOREIGN KEY `LaporanHarian_penugasanId_fkey`;
+ALTER TABLE `LaporanHarian` DROP FOREIGN KEY `LaporanHarian_penugasanId_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `LaporanHarian` ADD CONSTRAINT `LaporanHarian_penugasanId_fkey` FOREIGN KEY (`penugasanId`) REFERENCES `Penugasan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
